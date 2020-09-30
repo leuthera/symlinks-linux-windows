e899c91f0bcb1ca0cc973c4b87611c725ea86d8a
Antonio Leuther <leuther@strato.de>
2020-09-30 10:19:25 +0200

    drymode

--- fix-symlinks.sh
+++ fix-symlinks.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
-server=$1
+ssh_server=$1
 server_path=$2
+dry=$3
 
 for symlink in $(git ls-files -s | gawk '/12000/{print $4}');
 do
     target=$(<$symlink)
-    ssh $ssh_server -t "cd $server_path; ln -s $target $symlink"
+    if [ -n $dry ]; then
+        echo "fixing w/ ssh $ssh_server -t 'cd $server_path; ln -s $target $symlink'"
+    else
+        ssh $ssh_server -t "cd $server_path; ln -s $target $symlink"
+    fi
 done
