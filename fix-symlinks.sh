da417a352f7566ebc667bfa8933b6763aed382ea
Antonio Leuther <leuther@strato.de>
2020-09-30 10:41:14 +0200

    force is needed to overwrite existing files

--- fix-symlinks.sh
+++ fix-symlinks.sh
@@ -6,9 +6,9 @@ dry=$3
 for symlink in $(git ls-files -s | gawk '/12000/{print $4}');
 do
     target=$(<$symlink)
-    if [ -n $dry ]; then
-        echo "fixing w/ ssh $ssh_server -t 'cd $server_path; ln -s $target $symlink'"
+    if [ -n "$dry" ]; then
+        echo "fixing w/ ssh $ssh_server -t 'cd $server_path; ln -sf $target $symlink'"
     else
-        ssh $ssh_server -t "cd $server_path; ln -s $target $symlink"
+        ssh $ssh_server -t "cd $server_path; ln -sf $target $symlink"
     fi
 done
